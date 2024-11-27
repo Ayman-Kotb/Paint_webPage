c5a06479a0a15faad6cbfedf00a6a035cd701991
braa mostafa <braamostafa1@gmail.com>
2024-11-27 19:06:56 +0200

    added a toolbox to change the canvas dimensions

--- src/components/myCanvas/CanvasComponent.jsx
+++ src/components/myCanvas/CanvasComponent.jsx
@@ -1,5 +1,5 @@
 
-import {useRef, useEffect} from "react"
+import {useState, useEffect} from "react"
 import {Canvas} from "fabric"
 import "./CanvasComponent.css"
 
@@ -37,9 +37,49 @@ function CanvasComponent({canvas}){
                 startCanvas.dispose();
             }
     }, []);
+    const [height, setHeight] = useState(450);
+    const [width, setWidth] = useState(900);
 
+    const changeHeight = (e) => {
+        const theHeight = Number (e.target.value);
+        setHeight(theHeight);
+        canvas.current.setHeight(theHeight);
+        canvas.current.renderAll();
+    }
+    const changeWidth = (e) => {
+        const theWidth = Number (e.target.value);
+        setWidth(theWidth);
+        canvas.current.setWidth(theWidth);
+        canvas.current.renderAll();
+    }
     return(
-        <canvas id="canvas"/>
+        <div className="canvasContainer">
+            <canvas id="canvas"/>
+            <div className="canvasAdjustSize">
+                <p>Change canvas size</p>
+                <label>height: </label>
+                <input 
+                    type="range"
+                    min="100" 
+                    max="700" 
+                    step="1" 
+                    value={height} 
+                    onChange={(e)=>changeHeight(e)}
+                />
+                <label>{height}</label>
+                <br/>
+                <label>width: </label>
+                <input 
+                    type="range"
+                    min="100" 
+                    max="1400" 
+                    step="1" 
+                    value={width} 
+                    onChange={(e)=>changeWidth(e)}
+                />
+                <label> {width}</label>
+            </div>
+        </div>
     )  
 }
 export default CanvasComponent
