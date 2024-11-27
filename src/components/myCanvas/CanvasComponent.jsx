e4a77697512b2043f005490a2e7aa4ba59ec2afd
abdo <you@example.com>
2024-11-27 14:21:50 +0200

    .

--- src/components/myCanvas/CanvasComponent.jsx
+++ src/components/myCanvas/CanvasComponent.jsx
@@ -1,70 +1,45 @@
-import { useRef, useEffect } from "react";
-import { Canvas, Group } from "fabric";
-import "./CanvasComponent.css";
 
-function CanvasComponent({ canvas }) {
-    useEffect(() => {
-        const startCanvas = new Canvas("canvas", {
-            height: 450,
-            width: 900,
-        });
+import {useRef, useEffect} from "react"
+import {Canvas} from "fabric"
+import "./CanvasComponent.css"
 
-        canvas.current = startCanvas;
-
-        // Existing feature for path creation
-        canvas.current.on('path:created', (event) => {
-            const path = event.path;
-            path.set({
-                selectable: true,
-                evented: true,
-                hasControls: true,
-                hasBorders: true,
-                lockScalingX: false,
-                lockScalingY: false,
-                lockRotation: false,
+function CanvasComponent({canvas}){
+        useEffect(()=>{
+            const startCanvas = new Canvas("canvas",{
+                height: 450,
+                width: 900,
             });
-            if (!path.findControl) {
-                path.findControl = () => null;
-            }
-            if (!path.onDragStart) {
-                path.onDragStart = () => {};
-            }
-            if (!path.shouldStartDragging) {
-                path.shouldStartDragging = () => false;
-            }
-            canvas.current.add(path);
-            canvas.current.renderAll();
-        });
-
-        
-        startCanvas.on("selection:created", (event) => {
-            const selectedObjects = event.selected;
-
-            if (selectedObjects.length > 1) {
-          
-                const group = new Group(selectedObjects, {
+            canvas.current = startCanvas;
+            canvas.current.on('path:created', (event) => {
+                const path = event.path;
+                path.set({
                     selectable: true,
                     evented: true,
+                    hasControls: true,
+                    hasBorders: true,
+                    lockScalingX: false,
+                    lockScalingY: false,
+                    lockRotation: false,
                 });
-
-                
-                selectedObjects.forEach((object) => {
-                    startCanvas.remove(object);
-                });
-
-              
-                startCanvas.add(group);
-                startCanvas.setActiveObject(group);
-                startCanvas.renderAll();
+                if (!path.findControl) {
+                    path.findControl = () => null;
+                }
+                if (!path.onDragStart) {
+                    path.onDragStart = () => {};
+                }
+                if (!path.shouldStartDragging) {
+                    path.shouldStartDragging = () => false;
+                }
+                canvas.current.add(path);
+                canvas.current.renderAll();
+            });
+            return () =>{
+                startCanvas.dispose();
             }
-        });
-
-        return () => {
-            startCanvas.dispose();
-        };
     }, []);
 
-    return <canvas id="canvas" />;
+    return(
+        <canvas id="canvas"/>
+    )  
 }
-
-export default CanvasComponent;
+export default CanvasComponent
