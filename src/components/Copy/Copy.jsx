14e3fd5c7c8b6806ddd0b021e304c3833168ada0
braa mostafa <braamostafa1@gmail.com>
2024-11-28 16:21:48 +0200

    not yet.

--- src/components/Copy/Copy.jsx
+++ src/components/Copy/Copy.jsx
@@ -8,26 +8,48 @@ function Copy({canvas}){
 
       if(active){
 
-        if (active instanceof fabric.Path) {
-          active.clone((clonedPath) => {
-            canvas.current.add(clonedPath);
+          if (active instanceof fabric.Path) {
+            const jsonRepresentation = active.toObject(['shouldStartDragging', 'getActiveControl', 'onDragStart', 'canDrop']); // Include custom properties
+
+            fabric.util.enlivenObjects([jsonRepresentation], ([clonedObject]) => {
+
+              clonedObject.shouldStartDragging = active.shouldStartDragging || (() => false);
+              clonedObject.getActiveControl = active.getActiveControl || (() => null);
+              clonedObject.onDragStart = active.onDragStart || (() => {});
+              clonedObject.canDrop = active.canDrop || (() => false);
+
+              Object.assign(clonedObject, {
+                ...active, 
+                left: active.left + 10, 
+                top: active.top + 10,
+              });
+          
+              
+              canvas.current.add(clonedObject);
+        
+              canvas.current.setActiveObject(clonedObject);
+          
+          
+              console.log(canvas.current.getObjects());
+            });
+            return ;
+          }
+        
+
+          const clonedShape = protoFac.createShape({
+            shape: active.type === "rect"? "rectangle": active.type,
+            color : active.get("stroke"),
+            height: active.get("height")*active.get("scaleY"),
+            width: active.get("width")*active.get("scaleX"),
+            radius: active.get("radius"),
+            strokeWidth: active.get("strokeWidth"),
+            fillColor: active.get("fill")
           });
-          return;
-        }
 
-        const clonedShape = protoFac.createShape({
-                            shape: active.type === "rect"? "rectangle": active.type,
-                            color : active.get("stroke"), height: active.get("height")*active.get("scaleY"),
-                            width: active.get("width")*active.get("scaleX"),
-                            radius: active.get("radius"),
-                            strokeWidth: active.get("strokeWidth"),
-                            fillColor: active.get("fill")
-                          });
-
-        canvas.current.add(clonedShape)
-        canvas.current.setActiveObject(clonedShape)
-        canvas.current.renderAll()
-      }
+canvas.current.add(clonedShape)
+canvas.current.setActiveObject(clonedShape)
+canvas.current.renderAll()
+}
     }
     return(
       <div className="container">
