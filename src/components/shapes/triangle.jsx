a94b501c0b99aa05d385fac27176048b7285f066
Ayman_Kotb <153757251+Ayman-Kotb@users.noreply.github.com>
2024-11-29 19:19:38 +0200

    small bug in Triangle

--- src/components/shapes/triangle.jsx
+++ src/components/shapes/triangle.jsx
@@ -2,7 +2,7 @@ import { Triangle } from "fabric"
 import Shape from "./Shape"
 
 class myTriangle extends Shape {
-    create(color , width=100 , height=100, strokeWidth=3 , fillColor="white"){
+    create(color , width=100 , height=200, strokeWidth=3 , fillColor="white"){
         return new Triangle({
             top: 100,
             left: 100,
