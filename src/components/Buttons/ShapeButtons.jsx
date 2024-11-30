47a1fd5ae2927246ae9823aacb4e090802ddeb02
abdo <you@example.com>
2024-11-30 04:01:37 +0200

    Set the line to the Fabric.lne

--- src/components/Buttons/ShapeButtons.jsx
+++ src/components/Buttons/ShapeButtons.jsx
@@ -1,11 +1,11 @@
-import { makeLine } from "./Line";
+
 
 function ShapeButtons({ canvas, addShape, color, setIsLine, lineRef }) {
   return (
     <>
       <button onClick={() => addShape({ typeOfShape: "circle" })} title="Circle" className="button">⚫</button>
       <button onClick={() => addShape({ typeOfShape: "ellipse" })} title="Ellipse" className="button">⬭</button>
-      <button onClick={() => makeLine(canvas.current, color, setIsLine, lineRef)} title="Line" className="button">📏</button>
+      <button onClick={() => addShape({ typeOfShape: "line" })} title="Triangle" className="button">📏</button>
       <button onClick={() => addShape({ typeOfShape: "rectangle" })} title="Rectangle" className="button">█</button>
       <button onClick={() => addShape({ typeOfShape: "square" })} title="Square" className="button">⬛</button>
       <button onClick={() => addShape({ typeOfShape: "triangle" })} title="Triangle" className="button">🔺</button>
